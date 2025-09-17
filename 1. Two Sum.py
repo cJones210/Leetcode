74dddb4ab2dfbd836950b6570143c19dda29ca58
cJones210 <crman210@gmail.com>
2025-09-17 15:47:19 -0700

    inital commit

---
+++ 1. Two Sum.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen={}
+        for i in range(len(nums)):
+            wanted = target - nums[i]
+            if wanted in seen:
+                return [seen[wanted], i]
+            seen[nums[i]] = i
