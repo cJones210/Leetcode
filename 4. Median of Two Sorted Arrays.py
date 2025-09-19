8b4347ccce5d1459ad6747e702ab56141c113fea
cJones210 <crman210@gmail.com>
2025-09-19 16:03:57 -0700

    not the fastest way but still 0ms

---
+++ 4. Median of Two Sorted Arrays.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        mixed=sorted(nums1+nums2)
+        mixedlen=len(mixed)
+        if mixedlen % 2 == 0:
+            even = float(mixed[mixedlen/2]+mixed[mixedlen/2-1])
+            if even == 0:
+                return(0)
+            else:
+                return(even/2)
+        else:
+            return(mixed[mixedlen/2])        
+#I understand this isnt the algorithmically fastest solution but it is still relatively fast for only needing to merge sort
+
+#0ms Beats 100.00%
